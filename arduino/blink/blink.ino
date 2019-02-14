084bc8d3956d42b99d892157e110bfd966badb93
Matthew Harrison <m.harrison@students.chapelfield.org>
2019-02-14 12:01:55 -0500

    Add comments to each line of code

--- arduino/blink/blink.ino
+++ arduino/blink/blink.ino
@@ -1,29 +1,29 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT); // Sets pin 13 as an output for this program
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);
-  delay(600);
-  digitalWrite(13, LOW);
-  delay(100); 
-  digitalWrite(13, HIGH);
-  delay(600);
-  digitalWrite(13, LOW);
-  delay(100);
-  digitalWrite(13, HIGH);
-  delay(200);
-  digitalWrite(13, LOW);
-  delay(100);
-   digitalWrite(13, HIGH);
-  delay(200);
-  digitalWrite(13, LOW);
-  delay(100);
-   digitalWrite(13, HIGH);
-  delay(600);
-  digitalWrite(13, LOW);
-  delay(100);
+  digitalWrite(13, HIGH); // turns LED on
+  delay(600);  // Specifies amount of time to run digitalWrite command
+  digitalWrite(13, LOW);  // Turns LED off
+  delay(100);   // Specifies amount of time to run digitalWrite command
+  digitalWrite(13, HIGH); // turns LED on
+  delay(600);  // Specifies amount of time to run digitalWrite command
+  digitalWrite(13, LOW);  // Turns LED off
+  delay(100);  // Specifies amount of time to run digitalWrite command
+  digitalWrite(13, HIGH); // turns LED on
+  delay(200);  // Specifies amount of time to run digitalWrite command
+  digitalWrite(13, LOW);  // Turns LED off
+  delay(100);  // Specifies amount of time to run digitalWrite command
+   digitalWrite(13, HIGH); // turns LED on
+  delay(200);  // Specifies amount of time to run digitalWrite command
+  digitalWrite(13, LOW);  // Turns LED off
+  delay(100);  // Specifies amount of time to run digitalWrite command
+   digitalWrite(13, HIGH); // turns LED on
+  delay(600);  // Specifies amount of time to run digitalWrite command
+  digitalWrite(13, LOW);  // Turns LED off
+  delay(100);  // Specifies amount of time to run digitalWrite command
 }
