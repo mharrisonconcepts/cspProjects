15524f824357d1c479052b75f4e0009c7f28d0a1
Matthew Harrison <m.harrison@students.chapelfield.org>
2019-02-21 12:44:57 -0500

    Change pin output to 12

--- arduino/blink/blink.ino
+++ arduino/blink/blink.ino
@@ -1,29 +1,29 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT); // Sets pin 13 as an output for this program
+  pinMode(12, OUTPUT); // Sets pin 12 as an output for this program
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH); // turns LED on
+  digitalWrite(12, HIGH); // turns LED on
   delay(600);  // Specifies amount of time to run digitalWrite command
-  digitalWrite(13, LOW);  // Turns LED off
+  digitalWrite(12, LOW);  // Turns LED off
   delay(100);   // Specifies amount of time to run digitalWrite command
-  digitalWrite(13, HIGH); // turns LED on
+  digitalWrite(12, HIGH); // turns LED on
   delay(600);  // Specifies amount of time to run digitalWrite command
-  digitalWrite(13, LOW);  // Turns LED off
+  digitalWrite(12, LOW);  // Turns LED off
   delay(100);  // Specifies amount of time to run digitalWrite command
-  digitalWrite(13, HIGH); // turns LED on
+  digitalWrite(12, HIGH); // turns LED on
   delay(200);  // Specifies amount of time to run digitalWrite command
-  digitalWrite(13, LOW);  // Turns LED off
+  digitalWrite(12, LOW);  // Turns LED off
   delay(100);  // Specifies amount of time to run digitalWrite command
-   digitalWrite(13, HIGH); // turns LED on
+   digitalWrite(12, HIGH); // turns LED on
   delay(200);  // Specifies amount of time to run digitalWrite command
-  digitalWrite(13, LOW);  // Turns LED off
+  digitalWrite(12, LOW);  // Turns LED off
   delay(100);  // Specifies amount of time to run digitalWrite command
-   digitalWrite(13, HIGH); // turns LED on
+   digitalWrite(12, HIGH); // turns LED on
   delay(600);  // Specifies amount of time to run digitalWrite command
-  digitalWrite(13, LOW);  // Turns LED off
+  digitalWrite(12, LOW);  // Turns LED off
   delay(100);  // Specifies amount of time to run digitalWrite command
 }
