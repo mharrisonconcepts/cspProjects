46e7d3aaaa0547d9610713f4d1b16488279b6d0c
Matthew Harrison <m.harrison@students.chapelfield.org>
2019-02-28 11:36:36 -0500

    Add serial feedback to serialBlink

--- arduino/serialBlink/serialBlink.ino
+++ arduino/serialBlink/serialBlink.ino
@@ -1,4 +1,5 @@
 void setup() {
+  Serial.begin(9600);
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(13, OUTPUT);
 }
@@ -6,7 +7,9 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.println("On");
   delay(1000);                       // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  Serial.println("Off");
   delay(1000);                       // wait for a second
 }
