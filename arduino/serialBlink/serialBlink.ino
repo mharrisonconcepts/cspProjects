0ea9404e6e74fc99fece2a000cba0c031b658653
Matthew Harrison <m.harrison@students.chapelfield.org>
2019-02-28 11:42:20 -0500

    Add startup message to serialBlink

--- arduino/serialBlink/serialBlink.ino
+++ arduino/serialBlink/serialBlink.ino
@@ -2,14 +2,15 @@ void setup() {
   Serial.begin(9600);
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(13, OUTPUT);
+  Serial.println("Hello, How are you?");  //tells the serial port to display the message "Hello, How are you?"
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  Serial.println("On");
+  Serial.println("On");   //tell the serial port to display the message "On"
   delay(1000);                       // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  Serial.println("Off");
+  Serial.println("Off");    //tell the serial port to display the message "Off"
   delay(1000);                       // wait for a second
 }
