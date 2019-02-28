49ccad745df0d9c7106d3887a209a55bad0a0ddc
Matthew Harrison <m.harrison@students.chapelfield.org>
2019-02-28 11:47:56 -0500

    Add instructions for control to serialBlink

--- arduino/serialBlink/serialBlink.ino
+++ arduino/serialBlink/serialBlink.ino
@@ -3,8 +3,9 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(13, OUTPUT);
   Serial.println("Hello, How are you?");  //tells the serial port to display the message "Hello, How are you?"
+  Serial.println("Send 'On' to turn on the LED.");
+  Serial.println("Send 'Off' to turn off the LED.");
 }
-
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
