dbeed421720cb96145c73eb3ba2ff8993d5b48a6
Matthew Harrison <m.harrison@students.chapelfield.org>
2019-04-04 12:04:43 -0400

    Add comments

--- arduino/ifFade/ifFade.ino
+++ arduino/ifFade/ifFade.ino
@@ -1,13 +1,13 @@
-int lightValue = 0;
-int delta = 1;
+int lightValue = 0; //sets "lightValue" to 0 at the beginning of the program
+int delta = 1; //sets "delta" to 1
 
-void setup() {
-  Serial.begin(9600);
-  Serial.setTimeout(10);
-  pinMode(9, OUTPUT);
+void setup() { //runs code once
+  Serial.begin(9600); //sets serial port as output
+  Serial.setTimeout(10); //tells the serial buffer to refresh every 10 milliseconds
+  pinMode(9, OUTPUT); //
 }
 
-void loop() {
+void loop() { //runs code repeatedly
   lightValue = lightValue + delta;
   if (lightValue <= 0 || lightValue >= 255) {
     delta = -delta;
