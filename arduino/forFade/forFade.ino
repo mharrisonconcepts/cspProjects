786c7b0f334245c563dfef5fe6b386c19240397f
Matthew Harrison <m.harrison@students.chapelfield.org>
2019-04-04 11:45:06 -0400

    Add code

--- arduino/forFade/forFade.ino
+++ arduino/forFade/forFade.ino
@@ -5,6 +5,14 @@ void setup() {
 }
 
 void loop() {
- 
-
+  for (int lightValue = 0; lightValue <= 255; lightValue++) {
+    analogWrite(9, lightValue);
+    Serial.println(lightValue);
+    delay(10);
+  }
+  for (int lightValue = 255; lightValue >= 0; lightValue--) {
+    analogWrite(9, lightValue);
+    Serial.println(lightValue);
+    delay(10);
+  }
 }
