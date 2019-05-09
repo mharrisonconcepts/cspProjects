4be1c54bf8c140d70d1f598aa5ec46c2ddc07b8f
Matthew Harrison <m.harrison@students.chapelfield.org>
2019-05-09 14:58:51 -0400

    Add readyToCalculate

--- arduino/serialCalculate/serialCalculate.ino
+++ arduino/serialCalculate/serialCalculate.ino
@@ -13,18 +13,21 @@ void setup() {
 void loop() {
   cleanData();
   parseData();
+  calculate();
 }
 void cleanData() {
   count = 0;
   while (Serial.available() > 0) {
     newData = true;
     if (isDigit(Serial.peek()) == true) {
+      delay(1);
       integers[count] = Serial.parseInt();
       count++;
       Serial.print("The current count is ");
       Serial.println(count);
     }
     else if (isGraph(Serial.peek()) == true) {
+      delay(1);
       sign = Serial.read();
       Serial.println(sign);
     }
@@ -41,5 +44,36 @@ void parseData() {
     Serial.println(sign);
     Serial.println(number2);
     newData = false;
+  }
+}
+void calculate() {
+  bool readyToCalculate = false;
+  bool readyToPrint = false;
+  if (readyToCalculate == true) {
+    Serial.println("readyToCalculate is running");
+    switch (sign) {
+      case '+':
+        result = number1 + number2;
+        readyToPrint = true;
+        Serial.println("+ broke");
+        break;
+      case '-':
+        result = number1 - number2;
+        readyToPrint = true;
+        Serial.println("- broke");
+        break;
+      case '*':
+        result = number1 * number2;
+        readyToPrint = true;
+        Serial.println("* broke");
+      case '/':
+        result = number1 / number2;
+        readyToPrint = true;
+        Serial.println("/ broke");
+      default:
+        Serial.println("This operation is not supported. Please try again.");
+        readyToPrint = false;
+        break;
+    }
   }
 }
